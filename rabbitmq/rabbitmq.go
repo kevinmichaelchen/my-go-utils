f3bfe4a2c6d133efe491f54a1d55345ffe2265e4
Kevin Chen <kevinmichaelchen@gmail.com>
2018-02-25 19:24:29 -0500

    Remove SendMessage

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -10,12 +10,6 @@ import (
 	"encoding/json"
 )
 
-func SendMessage(s Message) {
-	go func(ss Message) {
-		MessageQueue <- ss
-	}(s)
-}
-
 // Message represents an empty interface that will be marshaled to JSON and sent as a message.
 type Message string //interface{}
 
